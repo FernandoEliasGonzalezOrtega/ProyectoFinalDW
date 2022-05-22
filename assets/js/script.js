26ce4c5f387d5ea32a543459dd0ad6b1fae4e216
Eduardo Pérez Rosales <420090601@pcpuma.acatlan.unam.mx>
2022-05-21 23:44:56 -0500

    Update links

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,29 +7,29 @@ function actualizarTabla() {
             "idTrabajador": 1,
             "nombreTrabajador": "Tony Stark",
             "fechaTrabajador":1963,
-            "fotoTrabajador":"./img/person-circle.svg"
+            "fotoTrabajador":"./assets/img/person-circle.svg"
         },
         {
             "idTrabajador": 2,
             "nombreTrabajador": "Steve Rogers",
             "fechaTrabajador":1936,
-            "fotoTrabajador":"./img/person-circle.svg"
+            "fotoTrabajador":"./assets/img/person-circle.svg"
         },
         {
             "idTrabajador": 3,
             "nombreTrabajador": "Matt Murdock",
             "fechaTrabajador":1978,
-            "fotoTrabajador":"./img/person-circle.svg"
+            "fotoTrabajador":"./assets/img/person-circle.svg"
         },
         {
             "idTrabajador": 4,
             "nombreTrabajador": "Peter Parker",
             "fechaTrabajador":1996,
-            "fotoTrabajador":"./img/person-circle.svg"
+            "fotoTrabajador":"./assets/img/person-circle.svg"
         }
     ];
     console.log(trabajadores);
     for(let i=0; i<trabajadores.length; i++){
-        tabla.innerHTML += "<div class='row border-top border-bottom border-dark border-2'><div class='col-md-3 align-middle'><img src='"+trabajadores[i].fotoTrabajador+"' alt='prof-pic' height='100px' class='p-1 mx-5'></div><div class='col-md-6'><div class='row m-3'><div class='col-md-12'><div class='form-check'><input class='form-check-input' type='radio' name='trabajadores' id='trabajador1'><label class='form-check-label' for='trabajador1'>"+trabajadores[i].nombreTrabajador+"</label></div></div></div><div class='row m-3'><div class='col-md-6'>"+trabajadores[i].fechaTrabajador+"</div><div class='col-md-6'>"+trabajadores[i].idTrabajador+"</div></div></div><div class='col-md-3 align-middle'><a class='btn btn-primary btn-guardar m-4' href='./expediente.html'>Ver Datos</a></div></div>";
+        tabla.innerHTML += "<div class='row border-top border-bottom border-dark border-2'><div class='col-md-3 align-middle'><img src='"+trabajadores[i].fotoTrabajador+"' alt='prof-pic' height='100px' class='p-1 mx-5'></div><div class='col-md-3'><div class='row m-3'><div class='col-md-12'><div class='form-check'><input class='form-check-input' type='radio' name='trabajadores' id='trabajador1'><label class='form-check-label' for='trabajador1'>"+trabajadores[i].nombreTrabajador+"</label></div></div></div><div class='row m-3'><div class='col-md-6'>"+trabajadores[i].fechaTrabajador+"</div><div class='col-md-6'>"+trabajadores[i].idTrabajador+"</div></div></div><div class='col-md-3 align-middle'><a class='btn btn-primary btn-guardar m-4' href='./expediente.html'>Ver Datos</a></div><div class='col-md-3 align-middle'><input type='button' class='btn btn-primary btn-guardar' href="#"><i class='bi bi-file-earmark-person'></i></button></div></div>";
     }
 }
