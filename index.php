bb3e01842980a649cd6f1a5982da546bc2275261
mrboohdz <315234208@pcpuma.acatlan.unam.mx>
2022-05-22 03:34:28 -0500

    no

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-  session_start();
+/*  session_start();
 
   if (isset($_SESSION['user_id'])) {
     header('Location: ./empresasEdit.php');
-  }
+  } */
   require 'bd.php';
   if(isset($_POST['btnInicio'])){
     $usuario = $_POST['user'];
--- registro.php
+++ registro.php
@@ -1,8 +1,8 @@
 <?php
-  session_start();
+/*  session_start();
 if (!isset($_SESSION['user_id'])) {
   header('Location: ./index.php');
-}
+}*/
   require 'bd.php';
   $message = '';
   if(isset($_POST['btnRegistrar'])){
