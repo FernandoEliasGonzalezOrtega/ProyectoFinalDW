b7e99726ecb4af6a81b72e589030398b3c35e14e
mrboohdz <315234208@pcpuma.acatlan.unam.mx>
2022-06-02 12:13:31 -0500

    Update borrarEmpresa.php

--- borrarEmpresa.php
+++ borrarEmpresa.php
@@ -1,17 +1,14 @@
-<?php 
+<?php
     session_start();
-
     if (!isset($_SESSION['user_id'])) {
         header('Location: index.php');
         exit();
     }
-    
     include ("bd.php");
     $id=$_GET["id"];
     $sql = "UPDATE Empresa SET estatus=0 WHERE idEmpresa=$id";
     mysqli_query($conn, $sql);
     header('Location: empresasEdit.php');
-
     require './partials/historial.php';
     $des = "SE BORRÓ LA EMPRESA $id";
     nvo($des);
