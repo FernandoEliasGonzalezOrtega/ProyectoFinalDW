2a14363176cf7bb1a85b3c97d758645dd755132f
Miguel Torres <99153283+MrPicudo@users.noreply.github.com>
2022-06-02 12:50:48 -0500

    Update historial.php

--- partials/historial.php
+++ partials/historial.php
@@ -1,10 +1,13 @@
 <?php
   @session_start();
   function nvo($des){
+    //Esta función recibe una descripción de cada página donde se encuentra el ususario
     date_default_timezone_set('America/Mexico_City');
     require 'bd.php';
     $usuarioh = $_SESSION['user_id'];
     $fecha = date('Y-m-d H:i:s');
+    //Una vez tomado el usuario,la fecha exacta con hora y la descripción, se agrega
+    //al historial 
     $sql = "INSERT INTO Historial (usuario,fecha,descripcion) VALUES ('$usuarioh','$fecha','$des')";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
